eb742a379ed05544a7b407978d9022551cb214be
EmilyOIG <emlygghn@gmail.com>
2022-02-02 18:05:37 +0000

    Map shows up?

---
+++ parked/src/Map.js
@@ -0,0 +1,41 @@
+/**
+ * Defining a map functional component to use in our website
+ * Based on: https://dev.to/laney/react-mapbox-beginner-tutorial-2e35
+ * 
+ * Currently a temporary "dummy" map which centres on george square
+ * 
+ * TODO: load in and render geojson data
+ * TODO: stop from zooming? or havea  reset view button?
+ */
+
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+import './App.css';
+
+// mapboxgl.accessToken = ; //TODO: access key goes here 
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  // initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-3.188904, 55.943713],
+      zoom: 17,
+    });
+
+    // add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    // clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
+
+export default Map;
