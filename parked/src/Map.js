01e85957998a11526dce5056f5f48efd2ce6eb3d
EmilyOIG <emlygghn@gmail.com>
2022-02-02 20:50:47 +0000

    Loading Geojson layer onto map

--- parked/src/Map.js
+++ parked/src/Map.js
@@ -1,6 +1,9 @@
 /**
  * Defining a map functional component to use in our website
- * Based on: https://dev.to/laney/react-mapbox-beginner-tutorial-2e35
+ * Sources: 
+ * https://dev.to/laney/react-mapbox-beginner-tutorial-2e35
+ * https://docs.mapbox.com/mapbox-gl-js/example/multiple-geometries/
+ * https://docs.mapbox.com/mapbox-gl-js/example/geojson-line/
  * 
  * Currently a temporary "dummy" map which centres on george square
  * 
@@ -23,18 +26,41 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      // See style options here: https://docs.mapbox.com/api/maps/#styles
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-3.188904, 55.943713],
-      zoom: 16.5,
+      //west-end of the meadows
+      center: [-3.197310, 55.941542],
+      zoom: 16,
+      interactive: false
     });
 
-    // add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    map.on('load', () => {
+      map.addSource('park', {
+        'type': 'geojson',
+        'data': 'test.geojson' //test file of boundaries
+        });
+
+      // Add a map layer for each type of geometry
+      map.addLayer({
+        'id': 'park-boundary',
+        'type': 'line',
+        'source': 'park',
+        'layout': {
+          'visibility': 'visible',
+          'line-join': 'round',
+          'line-cap': 'round'
+          },
+        'paint': {
+          'line-color': 'blue',
+          'line-width': 5
+          },
+        'filter': ['==', '$type', 'LineString']
+      });
+    });
+      
 
     // clean up on unmount
     return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
   return <div className="map-container" ref={mapContainerRef} />;
 };
