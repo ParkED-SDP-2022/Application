957e1cb2e8f9b2fcb288eb593db26e48e5c14c2f
arehman1806 <arehmannasir@outlook.com>
2022-02-06 17:26:23 +0000

    centered the joystick

--- parked/src/pages/basic-controller/basic-controller.js
+++ parked/src/pages/basic-controller/basic-controller.js
@@ -6,20 +6,22 @@ import testMap from "../../assets/map/testmap.jpg"
 export class BasicController extends React.Component{
     render() {
         return(
-            <div className='container-fluid'>
+            <div className='page-container container-fluid text-white'>
             <div className='row d-flex justify-content-center align-items-center'>
               <strong className='display-9 '>Bench 1 Controller</strong>
               <p>Use the joystick below the move the robot</p>
             </div>
 
-              <div className='div1 row d-flex '>
-                <div className='col-8 d-flex flex-column justify-content-center align-items-center'>
-                  <h3>Map of Park</h3>
-                  <img src={testMap} alt="test image of map" width="600" height="350" />
+              <div className='div1 row'>
+                <div className='col-8 d-flex flex-column justify-content-start align-items-center'>
+                  <h3 className='mb-4'>Map of Park</h3>
+                  <img src={testMap} alt="test image of map"/>
                 </div>
-              <div className='col-4'>
-                <h3 className='' >Joystick Controller</h3>
-                <JoystickController />
+              <div className='col-4 d-flex flex-column justify-content-center align-items-center'>
+                <h3 className='mb-4' >Joystick Controller</h3>
+                <div className='mt-3'>
+                  <JoystickController/>
+                </div>
               </div>
             </div>
           </div>
