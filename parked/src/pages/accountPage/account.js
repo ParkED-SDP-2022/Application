d88570911d85b5b1b1f393539fe73a16d358daea
EmilyOIG <emlygghn@gmail.com>
2022-03-04 15:21:13 +0000

    Made autofilling form for motion

--- parked/src/pages/accountPage/account.js
+++ parked/src/pages/accountPage/account.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 import "./account.scss";
 import { JoystickController } from "../../components/joystick-controller/joystick-controller";
 import testMap from "../../assets/map/test.geojson";
@@ -6,22 +6,41 @@ import benches from "../../assets/map/benches.geojson";
 import Map from "../../components/map/Map";
 import Form from "../../components/form/form";
 
+export const CoordsContext = createContext({
+  coords: 1,
+  setCoords: () => {}
+});
+
 export class AccountPage extends React.Component{
-    render() {
-        return(
-            <div className='AccountPage'>
-                <div className='MapSide'>
-                  <Map parkBoundaries={testMap} benches={benches} className="map"/>
-                </div>
-              <div className='ControlSide'>
-                  <div className="MoveForm">
-                      <Form />
-                  </div>
-                  <div className="Joystick">
-                    <JoystickController/>
-                  </div>
+
+  state = {
+    benchID: "",
+    location: "",
+   }
+
+handleIDCallback = (bench) =>{
+    this.setState({benchID: bench})
+}
+
+handleLocationCallback = (loc) =>{
+  this.setState({location: loc})
+}
+
+  render() {
+      return(
+          <div className='AccountPage'>
+              <div className='MapSide'>
+                <Map parkBoundaries={testMap} benches={benches}  IDhandler={this.handleIDCallback} locHandler={this.handleLocationCallback} center={[-3.197310, 55.941542]} className="map"/>
               </div>
-          </div>
-        )
-    }
+            <div className='ControlSide'>
+                <div className="MoveForm">
+                    <Form benchID={this.state.benchID} loc={this.state.location}/>
+                </div>
+                <div className="Joystick">
+                  <JoystickController/>
+                </div>
+            </div>
+        </div>
+      )
+  }
 }
